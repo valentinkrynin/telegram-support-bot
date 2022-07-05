9d2089e1074bb87129f8eb7f62e9b1227bdfdb48
Valentyn Krynin <vk@knk.group>
2022-07-05 11:37:05 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -26,7 +26,7 @@ def forward_to_chat(update, context):
         'from': {'id': 49820636, 'first_name': 'Daniil', 'is_bot': False, 'last_name': 'Okhlopkov', 'username': 'danokhlopkov', 'language_code': 'en'}
     }"""
     forwarded = update.message.forward(chat_id=TELEGRAM_SUPPORT_CHAT_ID)
-    if not forwarded.forward_from:
+    """if not forwarded.forward_from:"""
         context.bot.send_message(
             chat_id=TELEGRAM_SUPPORT_CHAT_ID,
             reply_to_message_id=forwarded.message_id,
